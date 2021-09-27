6d40fd1c7dfdeedde51fdfbebb77f16d276fdcf6
Deepak-313NS <86565170+Deepak-313NS@users.noreply.github.com>
2021-09-28 00:13:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-// var modal = document.getElementById('deepakNS');
-// window.onclick = function (event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-
 var modal = document.getElementById('deepakNS');
-var modal1 = document.getElementById('name');
-var modal2 = document.getElementById('psw');
 window.onclick = function (event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-console.log(modal1);
-console.log(modal2);
+
+// var modal = document.getElementById('deepakNS');
+// var modal1 = document.getElementById('name');
+// var modal2 = document.getElementById('psw');
+// window.onclick = function (event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
+// console.log(modal1);
+// console.log(modal2);
