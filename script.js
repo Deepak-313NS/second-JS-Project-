409a90dbc801cbd3c6a4d38e68e1d305af9b1a0f
Deepak-313NS <86565170+Deepak-313NS@users.noreply.github.com>
2021-10-25 18:07:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,3 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
-
-// var modal = document.getElementById('deepakNS');
-// var modal1 = document.getElementById('name');
-// var modal2 = document.getElementById('psw');
-// window.onclick = function (event) {
-//     if (event.target == modal) {
-//         modal.style.display = "none";
-//     }
-// }
-// console.log(modal1);
-// console.log(modal2);
